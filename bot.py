451e202e131934096cbab2de3bc5c1a147f30deb
marcusfooo <43901768+marcusfooo@users.noreply.github.com>
2019-10-29 08:43:46 +0800

    Modularised code
    Modularised code from bot.py to utils.py

--- bot.py
+++ bot.py
@@ -1,33 +1,13 @@
-import csv
-import random
 import utils
 import telebot
 from telebot.types import ReplyKeyboardRemove, ReplyKeyboardMarkup
-import datetime
 
 # Bot Settings
 TOKEN = '712534091:AAGDpXymyg8wlAMvd7QEWwi9umNsRsXxUlE'
 bot = telebot.TeleBot(TOKEN)
 
-# CatchOfTheDay inputs
-with open('catch.csv') as csv_catch:
-    csv_reader_catch = csv.reader(csv_catch, delimiter=',')
-    for row in csv_reader_catch:
-        catch_list = row
-    catch = catch_list[0]
-    catch_price = catch_list[1]
-    csv_catch.close()
-
-
 # UI inputs
-shop_list = list(utils.today_store_func())
-every_store_list = ['MiniWok', 'Mcdonalds', 'KFC', 'The Sandwich Guys', 'Malay', 'Indian']
-storeSelect = ReplyKeyboardMarkup(one_time_keyboard=True)
-storeSelect.add('MiniWok', 'Mcdonalds', 'KFC', 'The Sandwich Guys', 'Malay', 'Indian')
-itemSelect = ReplyKeyboardMarkup(one_time_keyboard=True)
-for i in shop_list:
-    itemSelect.add(i)
-hideBoard = ReplyKeyboardRemove()
+hideBoard = ReplyKeyboardRemove()  # function to hide inline keyboard
 
 
 # Start Menu
@@ -36,105 +16,86 @@ def start_command(message):
     bot.send_message(
         message.chat.id,
         "What would you like to do today :)\n\n" +
-        "1) Press /CatchOfTheDay to view today's Special Offer\n" +
-        "2) Press /WaitingTime to calculate estimated queue time\n"+
-        "3) Press /MenuDisplay to see your menu of choice\n"+
-        "4) Press /OperatingHours to find the Operating hours for your desired shop\n"+
-        "5) Press /Voucher to get your daily voucher\n"
-        "6) Press /AboutUs to know more about our team!\n"
+        "A) Know more about our team! /AboutUs\n" +
+        "B) View today's Special Offer /CatchOfTheDay\n" +
+        "C) Display current Stall Menus /MenuDisplay\n" +
+        "Dx) Select date and time to get stall and menu\n" +
+        "E) Display estimated waiting time /WaitingTime\n" +
+        "F) Display Operating Hours for Stalls /OperatingHours\n" +
+        "G) Claim your daily voucher! /Voucher\n"
     )
 
 
 # Function for today's special deal
 @bot.message_handler(commands=['CatchOfTheDay'])
-def waitingTime(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    if catch == "Cheeseburger":
-        bot.send_photo(message.chat.id, open('cheeseburger.jpg', 'rb'))
-        bot.send_message(message.chat.id, catch + " is at " + catch_price + " only for today!"
-                         + "\nPress /start to return to main menu")
-
-    elif catch == "Big Mac":  # send the appropriate image based on the reply to the "/getImage" command
-        bot.send_photo(message.chat.id, open('bigmac.jpg', 'rb'))
-        bot.send_message(message.chat.id, catch + " is at " + catch_price + " only for today!"
-                         + "\nPress /start to return to main menu")
-
-    elif catch == "KFC Pocket":  # send the appropriate image based on the reply to the "/getImage" command
-        bot.send_photo(message.chat.id, open('pocket.jpg', 'rb'))
-        bot.send_message(message.chat.id, catch + " is at " + catch_price + " only for today!"
-                         + "\nPress /start to return to main menu")
+def catchoftheDay(message):
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
+    bot_photo, bot_response = utils.catchoftheday_func()  # Returns today's catch photo and response
+    bot.send_photo(message.chat.id, bot_photo)  # Send catch photo
+    bot.send_message(message.chat.id, bot_response)  # Send catch response
 
 
 # Call to calculate waiting time
 @bot.message_handler(commands=['WaitingTime'])
 def waitingTime(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    queue_number = bot.send_message(message.chat.id, "Enter number of people in queue\nor\n"
-                                                   "Press /start to return to main menu")
-    bot.register_next_step_handler(queue_number, calculate)
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
+    queue_number = bot.send_message(message.chat.id, "Enter number of people in queue\n"
+                                                     "or\n Press /start to return to main menu")
+    bot.register_next_step_handler(queue_number, calculate)  # Proceeds to calculate function after user input
 
 
 # Function to calculate waiting time
 def calculate(message):
-    queue_number = message.text
     try:
-        queue_number = int(queue_number)
-        calculated_time = queue_number * 5
-        hours = calculated_time // 60
-        minutes = calculated_time % 60
+        queue_number = int(message.text)
+        hours, minutes = utils.waiting_time_func(queue_number)  # Returns waiting time
         bot.send_message(message.chat.id, "The estimated queue time is %s hours and %s minutes." % (hours, minutes)
                          + "\n\n Press /start to return to main menu")
 
     except:
-        bot.send_message(message.chat.id,"Error, please send a number\nPress /WaitingTime to try again\n"
-                                         "or\n/start to pick another option")
+        bot.send_message(message.chat.id,"Error, please send a number\nPress /WaitingTime to try again "
+                                         "or /start to pick another option")
 
 
 # Call to MenuDisplay
 @bot.message_handler(commands=['MenuDisplay'])
 def getMenu(message):
+    shop_list = list(utils.today_store_func())  # Returns list of stores opened today
+    item_select = ReplyKeyboardMarkup(one_time_keyboard=True)  # Converts list of stores to keyboard
+    for i in shop_list:
+        item_select.add(i)
     cid = message.chat.id
-    bot.send_chat_action(message.chat.id, 'typing')
-    store_choices = bot.send_message(cid, "The stores opened today are: ", reply_markup=itemSelect)
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
+    store_choices = bot.send_message(cid, "The stores opened today are: "
+                                     , reply_markup=item_select)  # Provides user inline keyboard
     bot.register_next_step_handler(store_choices, menuSelect)
 
 
 # Finds the operating hours for store selected
 @bot.message_handler(commands=['OperatingHours'])
 def operatingHours(message):
-    bot.send_chat_action(message.chat.id, 'typing')
+    store_select = ReplyKeyboardMarkup(one_time_keyboard=True)
+    store_select.add('MiniWok', 'Mcdonalds', 'KFC', 'The Sandwich Guys', 'Malay', 'Indian')
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
     user_choice = bot.send_message(message.chat.id, "Which store's operating hours would you like to check? ",
-                                   reply_markup=storeSelect)
+                                   reply_markup=store_select)  # Provides user inline keyboard
     bot.register_next_step_handler(user_choice, storeFinder)
 
 
 # Call for voucher function
 @bot.message_handler(commands=['Voucher'])
 def voucher(message):
-    claimed_flag = 0
-    bot.send_chat_action(message.chat.id, 'typing')
-    user_name = str(message.from_user.username)#getusername
-    datecsvchecker()
-    lines = [line.rstrip('\n') for line in open('claimedvoucher.txt')]
-    if user_name in lines:
-        claimed_flag = 1
-
-    if claimed_flag == 0:
-        random_no = random.randint(1,5)
-        bot.send_message(message.chat.id, "Your Voucher is "+ str(random_no) + "\n\n Press /start to return to main menu")
-        with open('claimedvoucher.txt', 'a', newline='') as csv_voucherwrite:
-            csv_voucherwrite.write('\n' + user_name)
-            csv_voucherwrite.close()
-
-    elif claimed_flag == 1:
-        bot.send_message(message.chat.id, "You have already claimed your voucher for today."
-                         + "\n\n Press /start to return to main menu")
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
+    user_name = str(message.from_user.username)  # Retrieves username
+    utils.datecsvchecker()  # Checks if date in csv is today, if not, rewrite csv file
+    bot_response = utils.voucher_check(user_name)  # Returns voucher if available
+    bot.send_message(message.chat.id, bot_response)
 
 
 # Call for AboutUs function
 @bot.message_handler(commands=['AboutUs'])
 def voucher(message):
-    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
     bot.send_photo(message.chat.id, open('logo.jpg', 'rb'))
     bot.send_message(message.chat.id, "goGrub is a project for AY19/20 CZ1003.\n\n"
                      "Our team members consists of:"
@@ -145,61 +106,28 @@ def voucher(message):
                      + "\n\nPress /start to return to main menu")
 
 
-# Resets voucher claims for the day
-def datecsvchecker():
-    now = datetime.datetime.now()
-    current_date = now.strftime('%d:%m:%Y')
-    lines = [line.rstrip('\n') for line in open('claimedvoucher.txt')]
-    if current_date in lines:
-        pass
-    else:
-        with open('claimedvoucher.txt', 'w+') as csv_voucherwrite:
-            csv_voucherwrite.write("\n" + current_date)
-            csv_voucherwrite.close()
-
-
 # Followup function for MenuDisplay
 @bot.message_handler(func=lambda message: True)
 def menuSelect(message):
     cid = message.chat.id
     user_store_choice = message.text
-    bot.send_chat_action(cid, 'typing')
-    time_period = utils.time_check()
-    store_menu = ""
-    if user_store_choice in shop_list:
-        for row in utils.menu:
-            if utils.day == int(row[0]) and time_period == row[1] and user_store_choice == row[2]:
-                store_menu = store_menu + (row[3] + " " + row[4])
-                bot.send_message(chat_id=message.chat.id, text=user_store_choice + " currently has: " + store_menu
-                                 + "\n\n Press /start to return to main menu", reply_markup=hideBoard)
-        if store_menu == "":
-            bot.send_message(chat_id=message.chat.id, text=user_store_choice + " currently has nothing available."
-                                                + "\n\n Press /start to return to main menu", reply_markup=hideBoard)
-    else:
-        bot.send_message(message.chat.id, "Invalid input, please do try /MenuDisplay again"
-                         + "\n\n Press /start to return to main menu", reply_markup=hideBoard)
+    bot.send_chat_action(cid, 'typing')  # Bot typing action
+    bot_response = utils.menu_input_parser(user_store_choice)  # Returns selected store items and price
+    bot.send_message(message.chat.id, bot_response, reply_markup=hideBoard)  # Removes inline keyboard
 
 
 # Followup function for Operating hours
 @bot.message_handler(func=lambda message: True)
 def storeFinder(message):
     user_store_choice = message.text
-    opening_days = ""
-    bot.send_chat_action(message.chat.id, 'typing')
-    if user_store_choice in every_store_list:
-        for rows in utils.stores_opened:
-            if user_store_choice == rows[1]:
-                opening_days = opening_days + ("\n"+user_store_choice + " is open on " + rows[2])
-        bot.send_message(chat_id=message.chat.id, text=user_store_choice + " are opened on the following days: \n"
-                        + opening_days + "\n\n Press /start to return to main menu", reply_markup=hideBoard)
-    else:
-        bot.send_message(message.chat.id, "Invalid input, please do try /OperatingHours again"
-                         + "\n\n Press /start to return to main menu", reply_markup=hideBoard)
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
+    bot_response = utils.store_input_parser(user_store_choice)  # Returns selected store operating hours
+    bot.send_message(chat_id=message.chat.id, text= bot_response, reply_markup=hideBoard)  # Removes inline keyboard
 
 
-# Fallback message
+# Default fallback message
 def command_default(message):
-    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_chat_action(message.chat.id, 'typing')  # Bot typing action
     bot.send_message(message.chat.id, "I don't understand, please do try the command again"
                      + "\n\n Press /start to return to main menu")
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import random
 
 
 # Reads Opening Hours csv
@@ -24,7 +25,7 @@ current_time = datetime.datetime.strptime(current_time, '%H:%M:%S').time()
 
 # Matches user_time to breakfast/ lunch/ dinner/ closed
 def time_check():
-    if datetime.time(8, 0, 0) <= current_time <= datetime.time(11, 0, 0):
+    if datetime.time(6, 0, 0) <= current_time <= datetime.time(11, 0, 0):
         time_period = 'Breakfast'
         return time_period
     elif datetime.time(11, 0, 1) <= current_time <= datetime.time(17, 0, 0):
@@ -58,19 +59,50 @@ def menu_func():
             store_menu.append({row[3]: row[4]})
 
 
-# Feature E: Waiting Time calculator
-def waiting_time_func():
-    queue_number = input("Kindly input number of people in queue: ")
-    while True:
-        try:
-            queue_number = int(queue_number)
-            break
-        except:
-            queue_number = input("Kindly input number of people in queue: ")
+# Parses user store input
+def menu_input_parser(user_store_choice):
+    time_period = time_check()  # Checks for current time_period: Breakfast/ Lunch/ Dinner/ Closed
+    shop_list = list(today_store_func())
+    store_menu = ""
+    store_menu_list = []
+    if user_store_choice in shop_list:  # Return items if store is opened
+        for row in menu:
+            if day == int(row[0]) and time_period == row[1] and user_store_choice == row[2]:
+                store_menu = store_menu + (row[3] + " " + row[4] + "\n")
 
+        if store_menu == "":  # Return string if none available
+            return (user_store_choice + " " + time_period + " Menu currently has nothing available." +
+                    "\n\n Press /start to return to main menu")
+
+        else:
+            return (user_store_choice + " " + time_period + " Menu currently has: \n" + store_menu +
+                    "\n\n Press /start to return to main menu")
+
+    else:  # Return error message if input is invalid
+        return "Invalid input, please do try /MenuDisplay again \n\n Press /start to return to main menu"
+
+
+def store_input_parser(user_store_choice):
+    every_store_list = ['MiniWok', 'Mcdonalds', 'KFC', 'The Sandwich Guys', 'Malay', 'Indian']
+    opening_days = ""
+    if user_store_choice in every_store_list:
+        for rows in stores_opened:
+            if user_store_choice == rows[1]:
+                opening_days = opening_days + ("\n"+user_store_choice + " is open on " + rows[2])
+
+        return (user_store_choice + " are opened on the following days: \n" +
+                opening_days + "\n\n Press /start to return to main menu")
+
+    else:
+        return "Invalid input, please do try /OperatingHours again" + "\n\n Press /start to return to main menu"
+
+
+# Feature E: Waiting Time calculator
+def waiting_time_func(queue_number):
     calculated_time = queue_number * 5
     hours = calculated_time // 60
     minutes = calculated_time % 60
+    return hours, minutes
 
 
 # Feature F: Prints store opening hours
@@ -82,5 +114,56 @@ def operating_hours_func():
             print(user_store_choice + " is open on " + rows[2])
 
 
+# Resets csv file to today's date
+def datecsvchecker():
+    now = datetime.datetime.now()
+    current_date = now.strftime('%d:%m:%Y')
+    lines = [line.rstrip('\n') for line in open('claimedvoucher.txt')]
+    if current_date in lines:  # If current date is in csv, ignore
+        pass
+    else:  # Otherwise, rewrite whole csv file and write today's date
+        with open('claimedvoucher.txt', 'w+') as csv_voucherwrite:
+            csv_voucherwrite.write("\n" + current_date)
+            csv_voucherwrite.close()
 
 
+# Checks whether user has already claimed voucher for the day
+def voucher_check(user_name):
+    claimed_flag = 0  # Flag to verify whether user has already claimed voucher
+    lines = [line.rstrip('\n') for line in open('claimedvoucher.txt')]
+    if user_name in lines:
+        claimed_flag = 1
+
+    if claimed_flag == 0:  # If user has not claimed, return voucher number
+        random_no = random.randint(1, 5)
+        with open('claimedvoucher.txt', 'a', newline='') as csv_voucherwrite:
+            csv_voucherwrite.write('\n' + user_name)  # Update name in csv and close file
+            csv_voucherwrite.close()
+        return "Your Voucher is " + str(random_no) + "\n\n Press /start to return to main menu"
+
+    elif claimed_flag == 1:  # Otherwise, return message
+        return "You have already claimed your voucher for today.\n\n Press /start to return to main menu"
+
+
+# Returns catch of the day item and price
+def catchoftheday_func():
+    with open('catch.csv') as csv_catch:
+        csv_reader_catch = csv.reader(csv_catch, delimiter=',')
+        for row in csv_reader_catch:
+            catch_list = row
+        catch = catch_list[0]  # Returns today's catch item
+        catch_price = catch_list[1]  # Returns today's catch price
+        csv_catch.close()
+
+    # Returns catch of the day photo
+    if catch == "Cheeseburger":
+        bot_photo = open('cheeseburger.jpg', 'rb')
+
+    elif catch == "Big Mac":
+        bot_photo = open('bigmac.jpg', 'rb')
+
+    elif catch == "KFC Pocket":
+        bot_photo = open('pocket.jpg', 'rb')
+
+    bot_response = catch + " is at " + catch_price + " only for today!\nPress /start to return to main menu"
+    return bot_photo, bot_response
