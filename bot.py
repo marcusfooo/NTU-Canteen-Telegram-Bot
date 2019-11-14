91514eceba06cafb3d93f446e9e8163ce3a5aa32
shadyshade98 <43901768+shadyshade98@users.noreply.github.com>
2019-11-14 19:42:33 +0800

    update
    update

--- bot.py
+++ bot.py
@@ -30,6 +30,7 @@ menu_dict = {
     "/Voucher": "voucher"
 }
 
+
 # Start Menu
 @bot.message_handler(commands=['start'])
 def start_command(message):
@@ -275,12 +276,12 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    bot.set_webhook(url='https://ntu-canteen-app.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 bot.infinity_polling()
